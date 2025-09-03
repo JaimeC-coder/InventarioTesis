c1fa6daf53b4eb0f62f1308ecd4782b1fa106fa6
Eduardo L <centurionjaime@gmail.com>
2025-09-03 00:01:24 -0500

    fix: cambiar la estructura del layout de admin a app en la vista de perfil

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,4 +1,4 @@
-<x-admin-layout>
+<x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Profile') }}
@@ -42,4 +42,4 @@
             @endif
         </div>
     </div>
-</x-admin-layout>
+</x-app-layout>
