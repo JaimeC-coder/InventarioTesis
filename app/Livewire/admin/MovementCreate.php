9a54f1d54b52247fba7e1f422dbb52d6b01d5333
Eduardo L <centurionjaime@gmail.com>
2025-09-03 01:01:01 -0500

    fix: corregir el nombre del espacio de nombres a minúsculas en varios archivos de Livewire

--- app/Livewire/admin/MovementCreate.php
+++ app/Livewire/admin/MovementCreate.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Livewire\Admin;
+namespace App\Livewire\admin;
 
 use App\Models\Inventorie;
 use App\Models\Movement;
--- app/Livewire/admin/QuoteCreate.php
+++ app/Livewire/admin/QuoteCreate.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Livewire\Admin;
+namespace App\Livewire\admin;
 
 use App\Models\Customer;
 use App\Models\Product;
