36d2a7cc6b746933595e194e82ba01cb345f124f
Eduardo L <centurionjaime@gmail.com>
2025-09-02 23:37:38 -0500

    fix: comentar inclusiones de componentes en la vista admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -33,12 +33,7 @@
     <x-banner />
 
 
-    {{-- @include('layouts.includes.admin.siderbar') --}}
-
     <livewire:components.navigation-menu />
-
-
-    {{-- @include('components.admin.structurebody') --}}
 
 
 
