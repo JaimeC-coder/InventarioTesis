9893076cd270b010f1260cf941e80db43cd6582c
Eduardo L <centurionjaime@gmail.com>
2026-09-05 01:19:39 -0500

    fix: renombrar parámetros en componentes Livewire de edición de clientes y proveedores

--- resources/views/admin/suppliers/edit.blade.php
+++ resources/views/admin/suppliers/edit.blade.php
@@ -6,7 +6,7 @@
 
     <div
         class="w-full p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <livewire:admin.edit.supplier :supplier="$supplier" />
+        <livewire:admin.edit.supplier :models-supplier="$supplier" />
     </div>
 
 
